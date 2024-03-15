7c0c78f8bedfac156df16e37f0dc3ec6e3e7c87b
Joseph Yates <joeyatessecond@gmail.com>
2024-03-15 22:37:58 +0000

    Fix graphical bug in sdl2 display struct

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ use app::App;
 use crate::e0c6s46::*;
 
 fn main() {
-    // let app = app::app_pixels::PixelApp::new();
+    //let app = app::app_pixels::PixelApp::new();
     let app = app::app_sdl2::Sdl2App::new();
     let cpu: CPU = create_e06s46_cpu();
     let mut rom: Vec<u16> = Vec::new();
-    match read_rom("../tama.b") {
+    match read_rom("tama.b") {
         Ok(data) => rom = data,
         Err(err) => println!("Error: {}", err),
     }
